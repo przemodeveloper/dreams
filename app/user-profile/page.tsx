9208d85cbc544815fdefcc3f542eda48a5cdf54c
przemodeveloper <przemyslaw.gorecki93@gmail.com>
2025-01-02 22:32:46 +0100

    Add basic user-profile page

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import {
+  dreamOptions,
+  genderOptions,
+} from "@/components/DatingProfileForm/datingProfile.consts";
+import useAuthUser from "@/hooks/useAuthUser";
+
 export default function UserProfilePage() {
-  return <h1>User Profile Page</h1>;
+  const { user } = useAuthUser();
+
+  const dream = dreamOptions.find(
+    (option) => option.value === user?.dream
+  )?.label;
+
+  const gender = genderOptions.find(
+    (option) => option.value === user?.gender
+  )?.label;
+
+  return (
+    <div className="h-screen flex justify-center items-center flex-col w-2/3 md:w-1/3 mx-auto">
+      <h3 className="font-secondary border-b-2 mb-4 w-full">
+        {user?.username}, {user?.age}
+      </h3>
+
+      <div className="border-b-2 mb-4 w-full">
+        <p className="font-secondary font-bold">Bio</p>
+        <p className="font-secondary text-xl">{user?.bio}</p>
+      </div>
+
+      <div className="w-full">
+        <p className="font-secondary font-bold">Essentials</p>
+        <ul className="font-secondary flex space-x-2">
+          <li className="bg-gray-200 rounded-full w-fit px-2 py-1">{dream}</li>
+          <li className="bg-gray-200 rounded-full w-fit px-2 py-1">{gender}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
