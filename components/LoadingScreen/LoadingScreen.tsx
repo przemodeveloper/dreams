42269057f9ed85247347b94814fcf4f25bdd8f4e
przemodeveloper <przemyslaw.gorecki93@gmail.com>
2025-01-03 15:22:05 +0100

    Add spinner

--- components/LoadingScreen/LoadingScreen.tsx
+++ components/LoadingScreen/LoadingScreen.tsx
@@ -1,7 +1,10 @@
+import styles from "./LoadingScreen.module.css";
+
 export default function LoadingScreen() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center flex-col h-screen">
       <h1 className="font-primary mb-2">Dreams</h1>
+      <div className={styles.loader} />
     </div>
   );
 }
