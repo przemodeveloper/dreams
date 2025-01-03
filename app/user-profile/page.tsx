3286688d2c6bcc083d49e18e267fd7dcb5aa0d76
przemodeveloper <przemyslaw.gorecki93@gmail.com>
2025-01-03 13:51:35 +0100

    Fix flashing

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -23,32 +23,40 @@ export default function UserProfilePage() {
   )?.label;
 
   return (
-    <div className="h-screen flex justify-center items-center flex-col w-2/3 md:w-1/3 mx-auto">
-      <h3 className="font-secondary border-b-2 mb-4 w-full">
-        {user?.username}, {user?.age}
-      </h3>
+    <>
+      {user && (
+        <div className="h-screen flex justify-center items-center flex-col w-2/3 md:w-1/3 mx-auto">
+          <h3 className="font-secondary border-b-2 mb-4 w-full">
+            {user?.username}, {user?.age}
+          </h3>
 
-      <div className="border-b-2 mb-4 w-full">
-        <p className="font-secondary font-bold">Bio</p>
-        <p className="font-secondary text-xl">{user?.bio}</p>
-      </div>
+          <div className="border-b-2 mb-4 w-full">
+            <p className="font-secondary font-bold">Bio</p>
+            <p className="font-secondary text-xl">{user?.bio}</p>
+          </div>
 
-      <div className="w-full mb-4">
-        <p className="font-secondary font-bold">Dream</p>
-        <ul className="font-secondary flex space-x-2">
-          <li className="bg-gray-200 rounded-full w-fit px-2 py-1">{dream}</li>
-        </ul>
-      </div>
+          <div className="w-full mb-4">
+            <p className="font-secondary font-bold">Dream</p>
+            <ul className="font-secondary flex space-x-2">
+              <li className="bg-gray-200 rounded-full w-fit px-2 py-1">
+                {dream}
+              </li>
+            </ul>
+          </div>
 
-      <div className="w-full">
-        <p className="font-secondary font-bold">Essentials</p>
-        <ul className="font-secondary flex space-x-2">
-          <li className="bg-gray-200 rounded-full w-fit px-2 py-1">{gender}</li>
-          <li className="bg-gray-200 rounded-full w-fit px-2 py-1">
-            {orientation}
-          </li>
-        </ul>
-      </div>
-    </div>
+          <div className="w-full">
+            <p className="font-secondary font-bold">Essentials</p>
+            <ul className="font-secondary flex space-x-2">
+              <li className="bg-gray-200 rounded-full w-fit px-2 py-1">
+                {gender}
+              </li>
+              <li className="bg-gray-200 rounded-full w-fit px-2 py-1">
+                {orientation}
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
