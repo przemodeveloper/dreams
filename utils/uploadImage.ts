99c4c15f00cd8683dfdee99eeba63369d4164340
przemodeveloper <przemyslaw.gorecki93@gmail.com>
2025-01-05 16:14:40 +0100

    Add upload function

---
+++ utils/uploadImage.ts
@@ -0,0 +1,25 @@
+import { storage } from "@/firebase";
+import { ref, uploadBytes } from "firebase/storage";
+
+export async function uploadImage(
+  image: File,
+  imageRef: string,
+  userId: string
+) {
+  const extension = image?.name.split(".").pop();
+
+  if (!extension) {
+    return;
+  }
+
+  const imagesDirectoryRef = ref(
+    storage,
+    `images/${userId}/${imageRef}.${extension}`
+  );
+
+  try {
+    await uploadBytes(imagesDirectoryRef, image);
+  } catch (error) {
+    throw new Error((error as Error).message);
+  }
+}
