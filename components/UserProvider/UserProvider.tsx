0f174a403560f508c3a2102c07d785f258bb0884
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2025-04-05 18:25:56 +0700

    Memoize context value

--- components/UserProvider/UserProvider.tsx
+++ components/UserProvider/UserProvider.tsx
@@ -4,16 +4,17 @@ import useAuthUser from "@/hooks/useAuthUser";
 import LoadingScreen from "@/components/LoadingScreen/LoadingScreen";
 import { NotificationContextProvider } from "@/context/notification-context";
 import { UserContextProvider } from "@/context/user-context";
-
+import { useMemo } from "react";
 export default function UserProvider({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	const { user, loading } = useAuthUser();
+	const value = useMemo(() => ({ user, loading }), [user, loading]);
 
 	return (
-		<UserContextProvider value={{ user, loading }}>
+		<UserContextProvider value={value}>
 			<NotificationContextProvider>
 				{loading === "pending" && !user ? <LoadingScreen /> : children}
 			</NotificationContextProvider>
