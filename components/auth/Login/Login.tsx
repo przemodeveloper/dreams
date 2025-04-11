5edb8253d784c943598f6588f991c54a434cb53c
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2025-04-11 17:56:37 +0700

    Delete checking for verified user

--- components/auth/Login/Login.tsx
+++ components/auth/Login/Login.tsx
@@ -17,10 +17,6 @@ export default function Login() {
 			const result = await signInWithPopup(auth, provider);
 			const authUser = result.user;
 
-			if (!authUser.emailVerified) {
-				router.push(ROUTES.VERIFY_EMAIL);
-			}
-
 			const snapshot = await getSnapshot(authUser.uid);
 
 			if (snapshot.empty) {
