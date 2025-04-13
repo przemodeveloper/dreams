f1ed31f74697d6dfee4024e4f9ee3690b98593c4
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2025-04-13 17:27:31 +0700

    Fix issue with storage bucket

--- firebase.ts
+++ firebase.ts
@@ -7,13 +7,13 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "dreams-a672f.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_URL,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const STORAGE_BUCKET_URL = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_URL;
+const STORAGE_BUCKET_URL = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
 
 const app = initializeApp(firebaseConfig);
 
