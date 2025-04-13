ee5d09b681fe5967161c5fc34fa73f8f1f8d3d7b
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2025-04-13 22:23:35 +0700

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,36 @@ import "./globals.css";
 import { Roboto } from "next/font/google";
 import UserProvider from "@/components/auth/UserProvider/UserProvider";
 import Footer from "@/components/layout/Footer/Footer";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	manifest: "/manifest.json",
+	icons: [
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "16x16",
+			url: "/favicon-16x16.png",
+		},
+		{
+			rel: "apple-touch-icon",
+			sizes: "180x180",
+			url: "/apple-touch-icon.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "192x192",
+			url: "/android-chrome-192x192.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "512x512",
+			url: "/android-chrome-512x512.png",
+		},
+	],
+};
 
 const roboto = Roboto({
 	subsets: ["latin"],
