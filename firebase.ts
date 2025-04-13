4004435d8492097a97ea447ade17cb67fe2eac33
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2025-04-13 16:59:30 +0700

    Bring back projectId

--- firebase.ts
+++ firebase.ts
@@ -5,7 +5,8 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "dreams--dreams-a672f.asia-east1.hosted.app", // Your App Hosting domain  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  authDomain: "dreams--dreams-a672f.asia-east1.hosted.app",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_URL,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
