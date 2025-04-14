5587075ba79b3eb505be01068e1963f3ba2f8e29
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2025-04-14 21:10:26 +0700

    Rename component

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,4 +1,10 @@
-export default function VerifyEmail() {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Verify your email",
+};
+
+export default function VerifyEmailPage() {
 	return (
 		<div className="flex flex-col items-center justify-center h-[calc(100vh-60px)]">
 			<h1 className="text-emerald-600 font-primary mb-2">Dreams</h1>
