c0ab388ed976b0627d38da39ccd171065a675656
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2025-04-19 15:40:32 +0700

    Protect meet-dreamers page

--- app/meet-dreamers/page.tsx
+++ app/meet-dreamers/page.tsx
@@ -1,10 +1,19 @@
 import MeetDreamersLayout from "@/components/meetDreamers/MeetDreamersLayout/MeetDreamersLayout";
+import { ROUTES } from "@/routes/routes";
+import { verifyIdToken } from "@/utils/auth";
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
 	title: "Meet Dreamers",
 };
 
-export default function MeetDreamersPage() {
+export default async function MeetDreamersPage() {
+	const user = await verifyIdToken();
+
+	if (!user) {
+		redirect(ROUTES.HOME);
+	}
+
 	return <MeetDreamersLayout />;
 }
