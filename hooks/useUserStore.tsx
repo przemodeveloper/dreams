57182dd3777c07054a7db40c8705e0cbf82a4c96
przemodeveloper <przemyslaw.gorecki93@gmail.com>
2025-11-11 21:04:36 +0100

    Restore code

--- hooks/useUserStore.tsx
+++ hooks/useUserStore.tsx
@@ -1,6 +1,7 @@
 import { LOADING_STATE, type LoadingState } from "@/constants/user-profile";
 import { auth, db } from "@/firebase";
 import type { UserProfile } from "@/lib/actions";
+import { setToken } from "@/lib/api/set-token";
 import type { User } from "firebase/auth";
 import type { FirestoreError } from "firebase/firestore";
 import { doc, onSnapshot } from "firebase/firestore";
@@ -22,6 +23,9 @@ export const useUserStore = create<UserStore>((set) => ({
     const unsubscribe = auth.onAuthStateChanged(async (authUser) => {
       if (authUser) {
         set({ authUser, loading: LOADING_STATE.PENDING });
+
+        const token = await authUser.getIdToken(true);
+        await setToken(token);
 
         // Subscribe to Firestore profile
         const userProfileDoc = doc(db, "profiles", authUser.uid);
