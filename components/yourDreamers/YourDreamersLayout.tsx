5c32c2d3087f0e9fea56ebbaa8fd787a3242403c
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2025-11-24 18:14:39 +0100

    Create layout

--- components/yourDreamers/YourDreamersLayout.tsx
+++ components/yourDreamers/YourDreamersLayout.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useUserStore } from "@/hooks/useUserStore";
+
 export default function YourDreamersLayout({ slug }: { slug: string }) {
+	const { matchedProfiles } = useUserStore((state) => state);
+
+	console.log(matchedProfiles);
+
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
 			<h1 className="text-2xl font-bold">YourDreamersLayout {slug}</h1>
